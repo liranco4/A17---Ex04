3ab88ce83c1029bb8cea623f6f2c2628b44b863f
liran cohen <liranco4@gmail.com>
2017-01-25 21:46:27 +0200

    more update

--- Ex04.Menus.Delegates/Ex04.Menus.Delegates/MenuWithInnerMenusCollection.cs
+++ Ex04.Menus.Delegates/Ex04.Menus.Delegates/MenuWithInnerMenusCollection.cs
@@ -21,21 +21,38 @@ namespace Ex04.Menus.Delegates
 
         internal override void ExecuteOperationOrShowInnerMenu()
         {
+            if(r_MenuCollection.Capacity != 0)
+            {
+                int menuIndex = 0;
+                StringBuilder menuBuilder = new StringBuilder();
+                menuBuilder.Append(string.Format("{0}. Back", menuIndex));
+                foreach(Menu menu in r_MenuCollection)
+                {
+                    menuBuilder.Append(string.Format("{0}. {1}", ++menuIndex, menu.MenuName));
+                }
+                
+                
+                
+                int userChoice = getAndcheckInputLegality();
 
+            }
         }
 
-        private int checkInputLegality(string i_Input)
+        private int getAndcheckInputLegality()
         {
             bool status = true;
             int result = -1;
+            Console.WriteLine("Please input your numeric choice in the following range: {0} - {1}", 0, r_MenuCollection.Capacity);
+            string input = Console.ReadLine();  
             do
             {
-                if (int.TryParse(i_Input, out result))
+                if (int.TryParse(input, out result))
                 {
                     if (result < 0 && result > r_MenuCollection.Capacity)
                     {
                         status = false;
                         Console.WriteLine("Invalid input!!! please input in the following range: {0} - {1}", 0, r_MenuCollection.Capacity);
+                        input = Console.ReadLine();
                     }
                 }
             }
