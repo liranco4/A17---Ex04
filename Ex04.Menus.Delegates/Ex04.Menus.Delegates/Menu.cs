c6ada55c2b3673f92ee633a1da0d1170355e08d1
liran cohen <liranco4@gmail.com>
2017-01-25 20:29:36 +0200

    new delegate project and Menu.cs absract

---
+++ Ex04.Menus.Delegates/Ex04.Menus.Delegates/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    internal abstract class Menu
+    {
+        protected string r_MenuName;
+        
+        Menu(string i_MenuName)
+        {
+            r_MenuName = i_MenuName;
+        }
+
+        public string MenuName
+        {
+            get
+            {
+               return r_MenuName;
+            }
+        }
+
+        protected abstract void ExecuteOperationOrShowInnerMenu();
+    }
+}
