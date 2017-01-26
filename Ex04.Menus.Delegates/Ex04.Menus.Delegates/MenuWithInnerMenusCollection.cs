adb4ab8a93183f618bf1dd505ce1cebb4e0110a9
liran cohen <liranco4@gmail.com>
2017-01-26 17:08:29 +0200

    new update for delegate

--- Ex04.Menus.Delegates/Ex04.Menus.Delegates/MenuWithInnerMenusCollection.cs
+++ Ex04.Menus.Delegates/Ex04.Menus.Delegates/MenuWithInnerMenusCollection.cs
@@ -27,15 +27,15 @@ namespace Ex04.Menus.Delegates
         internal override void ExecuteOperationOrShowInnerMenu()
         {
 			bool state = false;
-			if (r_MenuCollection.Capacity != 0)
+            Console.Clear();
+			if (r_MenuCollection.Count != 0)
 			{
 				do
 				{
-					Console.Clear();
 					int menuIndex = 0;
 					StringBuilder menuBuilder = new StringBuilder();
-					menuBuilder.Append(string.Format("{0}{1}___________________________", MenuName, Environment.NewLine));
-					menuBuilder.Append(string.Format("{0}. {1}", menuIndex , BackOrExitOperation()));
+					menuBuilder.Append(string.Format("{0}{1}___________________________{1}", MenuName, Environment.NewLine));
+					menuBuilder.Append(string.Format("{0}. {1}{2}", menuIndex , BackOrExitOperation(), Environment.NewLine));
 					foreach (Menu menu in r_MenuCollection)
 					{
 						menuBuilder.Append(string.Format("{0}. {1}{2}", ++menuIndex, menu.MenuName, Environment.NewLine));
@@ -45,6 +45,7 @@ namespace Ex04.Menus.Delegates
 					int userChoice = getAndcheckInputLegality();
 					if (userChoice != 0)
 					{
+                        Console.Clear();
 						r_MenuCollection[userChoice - 1].ExecuteOperationOrShowInnerMenu();
 					}
 					else
@@ -59,21 +60,22 @@ namespace Ex04.Menus.Delegates
 
         private int getAndcheckInputLegality()
         {
-            bool status = true;
+            bool status = false;
             int result = -1;
-            Console.WriteLine("Please input your numeric choice in the following range: {0} - {1}", 0, r_MenuCollection.Capacity);
+            Console.WriteLine("Please input your numeric choice in the following range: {0} - {1}", 0, r_MenuCollection.Count);
             string input = Console.ReadLine();  
             do
             {
-                if (int.TryParse(input, out result))
-                {
-                    if (result < 0 && result > r_MenuCollection.Capacity)
+                if (int.TryParse(input, out result) && result >= 0 && result <= r_MenuCollection.Count)
+                    {
+                        status = true;
+                    }
+                    else
                     {
                         status = false;
-                        Console.WriteLine("Invalid input!!! please input in the following range: {0} - {1}", 0, r_MenuCollection.Capacity);
+                        Console.WriteLine("Invalid input!!! please input in the following range: {0} - {1}", 0, r_MenuCollection.Count);
                         input = Console.ReadLine();
                     }
-                }
             }
             while (!status);
             return result;
--- Ex04.Menus.Delegates/Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Delegates/Ex04.Menus.Test/Program.cs
@@ -9,7 +9,8 @@ namespace Ex04.Menus.Test
     {
         public static void Main()
         {
-            RunInterfaceMenu();
+            RunDelegateMenu();
+            //RunInterfaceMenu();
         }
 
         private static void RunInterfaceMenu()
@@ -66,6 +67,10 @@ namespace Ex04.Menus.Test
 
 			charsCounterMenu.ExecuteMenuOperation += new CharsCount().ExecuteUserChoice;
 			countSpacesMenu.ExecuteMenuOperation += new CountSpaces().ExecuteUserChoice;
+
+            mainMenu.AddToCollection(dateTimeMenu);
+            mainMenu.AddToCollection(versionAndActionsMenu);
+            mainMenu.Show();
 		}
     }
 }
